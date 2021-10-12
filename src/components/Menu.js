35118e3a6d7229e738e7fd57d0aab8203e804c19
Damain Joseph <damain@wjatt.com>
2021-10-12 09:19:25 -0400

    feat: Add Addition Resources Link

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -12,12 +12,15 @@ function Menu({path}) {
         <Link className={`navbar-item ${path=== "/about/" ? "active" : ""}`}to="/about">
           ABOUT
         </Link>
+        <Link className={`navbar-item ${path=== "/blog/" ? "active" : ""}`}to="/blog">
+          ADDITIONAL RESOURCES
+        </Link>
         <Link className={`navbar-item ${path=== "/contact/" ? "active" : ""}`} to="/contact">
           CONTACT
         </Link>
-        <Link className={`navbar-item cta-button`} to="/contact/examples">
+        {/* <Link className={`navbar-item cta-button`} to="/contact/examples">
           SIGN THE PETITION
-        </Link>
+        </Link> */}
       </div>
     </>
   );
