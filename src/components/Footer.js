06ba9d9b9f40a9812cc0efc35960a0e17a4c3ede
Damain Joseph <damain@wjatt.com>
2021-10-12 08:55:33 -0400

    fix: changed contact details fixes #4

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -32,11 +32,11 @@ const Footer = ({ path }) => {
                         d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                       />
                     </svg>
-                    <span className="text">1 868 111 2222</span>
+                    <span className="text">1 868 356 6290</span>
                   </a>
                 </li>
                 <li>
-                  <a className="contact-item" href="mailto:info@ecowatchtt.com">
+                  <a className="contact-item" href="mailto:ecowatchtt@gmail.com">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       className="icon"
@@ -51,7 +51,7 @@ const Footer = ({ path }) => {
                         d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                       />
                     </svg>
-                    <span className="text">info@ecowatchtt.com</span>
+                    <span className="text">ecowatchtt@gmail.com</span>
                   </a>
                 </li>
               </ul>
