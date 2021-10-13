49ff454399188880c3e482ac2bf0a314746c2bb9
Damain Joseph <damain@wjatt.com>
2021-10-13 11:23:41 -0400

    feat: restrict amount of blog entries on home page changed style on hero image text

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -3,52 +3,59 @@ import PropTypes from "prop-types";
 import { Link, graphql, StaticQuery } from "gatsby";
 import PreviewCompatibleImage from "./PreviewCompatibleImage";
 
-const BlogRoll = ({ data , dataType = ""}) => {
+const BlogRoll = ({ data, count }) => {
   const { edges: posts } = data.allMarkdownRemark;
 
   return (
     <div className="columns is-multiline">
       {posts &&
-        posts.filter(({node:post})=>!post?.frontmatter?.tags?.includes("video")).map(({ node: post }) => (
-          <div className="is-parent column is-6" key={post.id}>
-            <article
-              className={`blog-list-item tile is-child box notification ${
-                post.frontmatter.featuredpost ? "is-featured" : ""
-              }`}
-            >
-              <div className="image">
-                {post.frontmatter.featuredimage ? (
-                  <div className="featured-thumbnail">
-                    <PreviewCompatibleImage
-                      imageInfo={{
-                        image: post.frontmatter.featuredimage,
-                        alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                      }}
-                    />
-                  </div>
-                ) : null}
-              </div>
-              <div className="blog-preview-content">
-                <header className="blog-preview-header">
-                  <h3 className="post-meta">
-                    <Link className="title has-text-primary is-size-4" to={post.fields.slug}>
-                      {post.frontmatter.title}
+        posts
+          .filter(({ node: post }) => !post?.frontmatter?.tags?.includes("video"))
+          .filter(({ node: post }, index) => {
+            if (!count) return true;
+            if (index < count) return true
+            return false
+          })
+          .map(({ node: post }) => (
+            <div className="is-parent column is-6" key={post.id}>
+              <article
+                className={`blog-list-item tile is-child box notification ${
+                  post.frontmatter.featuredpost ? "is-featured" : ""
+                }`}
+              >
+                <div className="image">
+                  {post.frontmatter.featuredimage ? (
+                    <div className="featured-thumbnail">
+                      <PreviewCompatibleImage
+                        imageInfo={{
+                          image: post.frontmatter.featuredimage,
+                          alt: `featured image thumbnail for post ${post.frontmatter.title}`,
+                        }}
+                      />
+                    </div>
+                  ) : null}
+                </div>
+                <div className="blog-preview-content">
+                  <header className="blog-preview-header">
+                    <h3 className="post-meta">
+                      <Link className="title has-text-primary is-size-4" to={post.fields.slug}>
+                        {post.frontmatter.title}
+                      </Link>
+                    </h3>
+                    <h4 className="date">{post.frontmatter.date}</h4>
+                  </header>
+                  <p>
+                    {post.excerpt}
+                    <br />
+                    <br />
+                    <Link className="button" to={post.fields.slug}>
+                      Keep Reading →
                     </Link>
-                  </h3>
-                  <h4 className="date">{post.frontmatter.date}</h4>
-                </header>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button" to={post.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </div>
-            </article>
-          </div>
-        ))}
+                  </p>
+                </div>
+              </article>
+            </div>
+          ))}
     </div>
   );
 };
@@ -61,7 +68,7 @@ BlogRoll.propTypes = {
   }),
 };
 
-export default () => (
+export default ({ count }) => (
   <StaticQuery
     query={graphql`
       query BlogRollQuery {
@@ -95,6 +102,6 @@ export default () => (
         }
       }
     `}
-    render={(data, count) => <BlogRoll data={data} count={count} />}
+    render={(data) => <BlogRoll data={data} count={count} />}
   />
 );
