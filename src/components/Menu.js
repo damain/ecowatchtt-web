cc827d5067defba88590109f00cb7333c6e5daa1
Damain Joseph <damain@wjatt.com>
2021-10-31 12:58:45 -0400

    updated lambda, activated contact menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -9,15 +9,15 @@ function Menu({path}) {
         <Link className={`navbar-item ${path=== "/" ? "active" : ""}`} to="/" >
           HOME
         </Link>
-        <Link className={`navbar-item ${path=== "/about/" ? "active" : ""}`}to="/about">
+        <Link className={`navbar-item ${path=== "/about/" ? "active" : ""}`} to="/about">
           ABOUT
         </Link>
-        <Link className={`navbar-item ${path=== "/blog/" ? "active" : ""}`}to="/blog">
+        <Link className={`navbar-item ${path=== "/blog/" ? "active" : ""}`} to="/blog">
           ADDITIONAL RESOURCES
         </Link>
-        {/* <Link className={`navbar-item ${path=== "/contact/" ? "active" : ""}`} to="/contact">
+        <Link className={`navbar-item ${path=== "/contact/" ? "active" : ""}`} to="/contact">
           CONTACT
-        </Link> */}
+        </Link>
         {/* <Link className={`navbar-item cta-button`} to="/contact/examples">
           SIGN THE PETITION
         </Link> */}
